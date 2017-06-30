97ce2fdbc8f53bba4f2b8ba4ea1bd539b1de887d
leo60228 <iakornfeld@gmail.com>
2017-06-30 11:46:38 -0400

    Improved deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,15 @@
 $ErrorActionPreference = "Stop"
 
 Push-Location $PSScriptRoot
+git diff-index --quiet HEAD --
+$unsaved = $LASTEXITCODE
+if ($unsaved) {git stash}
 git pull
 Remove-Item -Recurse -ErrorAction Ignore .\docs\
 Copy-Item -Recurse .\public\ .\docs\
 git add -f docs
 git commit -m '[SCRIPT] Updated site'
-Remove-Item -Recurse .\docs\
 git push
+if ($unsaved) {git stash pop}
 Pop-Location
 echo 'Deployed!'
